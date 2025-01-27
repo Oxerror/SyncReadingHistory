53a8ee59d8d168b5259cde10975e27e14fcdc600
Fabian Amhof <fabi.amhof@bluewin.ch>
2025-01-27 16:00:19 +0100

    use flat config style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,7 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {env: {node: true},}
+  ...tseslint.configs.recommended
 ];

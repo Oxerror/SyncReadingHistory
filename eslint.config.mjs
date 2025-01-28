ce31b3264475e2c01cdde3ba540bcc90bb7f6397
Fabian Amhof <fabi.amhof@bluewin.ch>
2025-01-28 09:01:15 +0100

    Update eslint / typescript config (#27)
    * Add empty line rule
    * update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.jest} }},
   ...tseslint.configs.recommended,
   pluginJs.configs.recommended,
+  {rules: {"no-multiple-empty-lines": [2, {"max": 1, "maxBOF": 0, "maxEOF": 1}]}},
+  {rules:{"@typescript-eslint/no-require-imports": "off"}}
 ];

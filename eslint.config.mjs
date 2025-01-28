f899dfe8b571e74568c1cb8ba29a87182c08cd1a
Fabian Amhof <fabi.amhof@bluewin.ch>
2025-01-28 08:58:50 +0100

    update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,6 @@ export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.jest} }},
   ...tseslint.configs.recommended,
   pluginJs.configs.recommended,
+  {rules: {"no-multiple-empty-lines": [2, {"max": 1, "maxBOF": 0, "maxEOF": 1}]}},
+  {rules:{"@typescript-eslint/no-require-imports": "off"}}
 ];
